669db7c03d7d695f311b5dec3afa61dcd49326a2
Pixselve <38262536+Pixselve@users.noreply.github.com>
2021-11-03 21:17:15 +0100

    :green_heart: fix host

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
     new FastifyAdapter(),
   );
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 
 bootstrap()
